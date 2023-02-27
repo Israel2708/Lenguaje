72e3b9abbdc56600fd298aaeaf760e3d5f6797f0
Israel2708 <126500313+Israel2708@users.noreply.github.com>
2023-02-27 17:23:52 +0100

    Add files via upload

---
+++ trabajo HTML_Israel_Azofra/Inicio-Sesion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio_Sesion</title>
+</head>
+<body>
+    <?php
+    var_dump($_REQUEST);
+    
+    ?>
+</body>
+</html>
---
+++ trabajo HTML_Israel_Azofra/usuario.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Nuevo_Usuario</title>
+</head>
+<body>
+    <?php
+    var_dump($_REQUEST);
+    
+    ?>
+</body>
+</html>
